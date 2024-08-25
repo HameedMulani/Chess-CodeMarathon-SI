7497aa0ec8dfb97a32a37111ad90b7c5f8ed33ed
HameedMulani <55029779+HameedMulani@users.noreply.github.com>
2024-08-26 01:20:52 +0530

    initial commit

---
+++ BackEnd/Program.cs
@@ -0,0 +1,43 @@
+
+using ChessApiDemo.DAO;
+using Chess_Hameed.Controllers;
+using Npgsql;
+namespace Chess_Hameed
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var connectionString = builder.Configuration.GetConnectionString("PostgreDB");
+            builder.Services.AddScoped((provider) => new NpgsqlConnection(connectionString));
+
+
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddScoped<IChessDAO, ChessDAOImpl>();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
